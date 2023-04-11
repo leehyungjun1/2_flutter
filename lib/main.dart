c4cc12b836cd7c5fdba922701be1b85a7ec8fde0
james <unpobby@nate.com>
2023-04-11 14:52:43 +0900

    Drawer Header modify

--- lib/main.dart
+++ lib/main.dart
@@ -31,24 +31,73 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
       appBar: AppBar(
         title: const Text('Pos Data'),
       ),
-      body: const Center(
-        child: ElevatedButton(
-          onPressed: null,
-          child: Text('Open Drawer'),
-        ),
+      body: const Row(
+        children: [
+          Text('이마트'),
+          Text('홈플러스'),
+        ],
       ),
       drawer: Drawer(
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              const Text('This is the Drawer'),
-              ElevatedButton(
-                onPressed: _closeDrawer,
-                child: const Text('Close Drawer'),
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            UserAccountsDrawerHeader(
+              currentAccountPicture: const CircleAvatar(
+                backgroundImage: AssetImage('assets/images/me.png'),
               ),
-            ],
-          ),
+              otherAccountsPictures: const [
+                CircleAvatar(
+                  backgroundImage: AssetImage('assets/images/2.jpg'),
+                  backgroundColor: Colors.white,
+                )
+              ],
+              accountName: const Text('James Lee'),
+              accountEmail: const Text('hjlee@ivenet.co.kr'),
+              onDetailsPressed: () {
+                print('arrow is clicked');
+              },
+              decoration: BoxDecoration(
+                color: Colors.red[200],
+                borderRadius: const BorderRadius.only(
+                  bottomLeft: Radius.circular(40.0),
+                  bottomRight: Radius.circular(40.0),
+                ),
+              ),
+            ),
+            ListTile(
+              leading: Icon(
+                Icons.home,
+                color: Colors.grey[850],
+              ),
+              title: const Text('Home'),
+              onTap: () {
+                print('Home is clicked');
+              },
+              trailing: const Icon(Icons.add),
+            ),
+            ListTile(
+              leading: Icon(
+                Icons.settings,
+                color: Colors.grey[850],
+              ),
+              title: const Text('Setting'),
+              onTap: () {
+                print('Setting is clicked');
+              },
+              trailing: const Icon(Icons.add),
+            ),
+            ListTile(
+              leading: Icon(
+                Icons.question_answer,
+                color: Colors.grey[850],
+              ),
+              title: const Text('Q&A'),
+              onTap: () {
+                print('Q&A is clicked');
+              },
+              trailing: const Icon(Icons.add),
+            ),
+          ],
         ),
       ),
     );
